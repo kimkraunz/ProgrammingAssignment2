4eba12cc95ae56ed6a01f2c7f0b251320ec734da
kimkraunz <kim.kraunz@gmail.com>
2015-08-21 09:24:03 -0700

    my cachematrix function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The makeCacheMatrix and cacheSolve functions return the index of a matrix, first by checking to see if there is a cached value and either returning the cached value or taking the inverse of the matrix and returning it.
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    ## Returns a vector that contains the functions to change the matrix (if        needed), return the matrix, store the value 'm', and return 'm' 
+    m <- NULL
+    set <- function(y) {                        
+        # changes the matrix (if needed) that is stored in the function by              defining 'x' and 'm' to the parent environment.  
+        x <<- y                              
+        m <<- NULL                              
+    }
+    
+    get <- function() x                         # returns the matrix 'x'
+    setinverse <- function(solve) m <<- solve   # stores the value of 'm'
+    getinverse <- function() m                  # returns the value of 'm'
+    list(set = set, get = get,                  # returns the functions as list
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+
+## This function returns the inverse of 'x'.  It checks to see whether there is ## a cached inverse.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()                 # sets 'm'
+    if(!is.null(m)) {                   # checks to see whether 'm' is null
+        message("getting cached data")
+        return(m)                       # if 'm' is not null, returns cached 'm'
+    }
+    data <- x$get()                     # else returns matrix 'x'
+    m <- solve(data, ...)               # returns the inverse of 'x'
+    x$setinverse(m)                     # stores the inverse
+    m                                   # returns the inverse of 'x' as 'm'
 }
